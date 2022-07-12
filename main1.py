798aebeba853e5675aec20e4e4ca12d2ae79896f
Максим Тимофеев <glowingfoxyt@gmail.com>
2022-07-12 23:40:15 +0500

    upload excel parser

--- main1.py
+++ main1.py
@@ -6,31 +6,49 @@ from matplotlib import cm
 import data
 
 
-y = data.y
-x = data.x
-z = data.z
+class InterpApp:
+    def __init__(self, method: str, lines: int):
+        self._takeData()
+        self.interp(method)
+        self.printPlot(lines)
 
-yi=np.linspace(min(y), max(y), 1000)
-xi=np.linspace(min(x), max(x), 1000)
+    def _takeData(self):
+        eData = data.DataExcel
+        eData._loadData(eData)
+        self.x = eData.x
+        self.y = eData.y
+        self.z = eData.z
 
-X, Y= np.meshgrid(xi,yi)
-Z = griddata((x, y), z, (X, Y), method='cubic')
-lvl = np.linspace(np.min(z), np.max(z), 25)
-lvl2 = [lvl[i] for i in range(5, len(lvl), 5)]
+    def interp(self, method):
+        yi=np.linspace(min(self.y), max(self.y), 1000)
+        xi=np.linspace(min(self.x), max(self.x), 1000)
 
-fig, ax = plt.subplots()
+        self.X, self.Y= np.meshgrid(xi,yi)
 
-ls = LightSource(275, 45)
-rgb = ls.shade(Z, cmap=cm.gist_earth, vert_exag=0.1, blend_mode='soft')
+        self.Z = griddata((self.x, self.y), self.z, (self.X, self.Y), method=method)
 
-cs = ax.contourf(X, Y, Z, lvl, facecolors=rgb, shade=False)
-cntr1 = ax.contour(X, Y, Z, lvl, colors='gray', extend='both', linestyle='dashed', linewidths=.5)
+    def printPlot(self, lines: int):
+        lvl = np.linspace(np.min(self.z), np.max(self.z), lines)
+        lvl2 = [lvl[i] for i in range(5, len(lvl), round(lines*0.2))]
 
-cntr2 = ax.contour(X, Y, Z, lvl2, colors='black', extend='both')
+        fig, ax = plt.subplots()
 
-ax.clabel(cntr2, levels=lvl2, fmt="%4i", fontsize='14',use_clabeltext=True)
-ax.set_title('Сплайн интерполяция кровли пласта')
+        ls = LightSource(275, 45)
+        rgb = ls.shade(self.Z, cmap=cm.gist_earth, vert_exag=0.1, blend_mode='soft')
+
+        cs = ax.contourf(self.X, self.Y, self.Z, lvl, facecolors=rgb, shade=False)
+        cntr1 = ax.contour(self.X, self.Y, self.Z, lvl, colors='black', extend='both', linewidths=.5)
+
+        cntr2 = ax.contour(self.X, self.Y, self.Z, lvl2, colors='black', extend='both')
+
+        ax.clabel(cntr2, levels=lvl2, fmt="%4i", fontsize='14',use_clabeltext=True)
+        ax.set_title('Сплайн интерполяция кровли пласта')
 
 
-fig.savefig('dd.pdf')
-plt.show()
+        fig.savefig('dd.pdf')
+        plt.show()
+
+
+if __name__ == '__main__':
+    app = InterpApp('cubic', 30)
+
